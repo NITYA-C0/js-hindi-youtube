558d6647a59e95cd27e5b40ee8c2b45afd0ddcf6
NITYA-C0 <154868587+NITYA-C0@users.noreply.github.com>
2025-05-06 06:25:42 +0000

    operations

---
+++ 01_basics/02_typeconversiondata.js
@@ -0,0 +1,11 @@
+// operations***
+let value=3
+let negativevalue=-value
+// console.log(negativevalue);
+console.log(+true);
+console.log(+"")
+// let num1,num2,num3
+// let num1=num2=num3=2+2
+let gamecounter=100
+gamecounter++;
+console.log (gamecounter);
