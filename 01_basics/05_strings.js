0e8ad532755725887254a3f53845ccd628d4b99b
NITYA-C0 <154868587+NITYA-C0@users.noreply.github.com>
2025-05-08 07:52:27 +0000

    your message

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,8 @@
+let name = "nitya" 
+let repocount = 50
+console.log(`hello my name is ${name} and my repcount is ${repocount}`);
+const gameName= new String('radha')
+console.log(gameName[0]);
+console.log(gameName. __proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
