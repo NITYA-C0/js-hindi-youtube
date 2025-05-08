ad4525cc6052472c8cb08d36ec1ec6da476d562c
NITYA-C0 <154868587+NITYA-C0@users.noreply.github.com>
2025-05-08 05:57:39 +0000

    datatypes-summary.js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,24 +1,41 @@
-//primitive
-//7 types: string, number, boolean,null, undefined,symbol,BigInt
-// refrence (nonprimitive)
-// arrays,objects,functions
-const score=100
-const scorevalue=100.3
-const isLoggedIn=false
-const outsideTemp=null
-let userEmail;
+// //primitive
+// //7 types: string, number, boolean,null, undefined,symbol,BigInt
+// // refrence (nonprimitive)
+// // arrays,objects,functions
+// const score=100
+// const scorevalue=100.3
+// const isLoggedIn=false
+// const outsideTemp=null
+// let userEmail;
 
-const id=symbol('123')
-const anotherId=symbol('123')
-console.log(id===anotherId)
-const bigNumber=345678986543322n
-const heros=["shaktiman", "naagraj","doga"]
-let myObj={
-    name:"nitya",
-    age:22
+// let mySymbol = Symbol("id");
+// console.log(typeof mySymbol); // symbol
+// console.log(id===anotherId)
+// const bigNumber=345678986543322n
+// const heros=["shaktiman", "naagraj","doga"]
+// let myObj={
+//     name:"nitya",
+//     age:22
+// }
+// const myFunction = function()
+// {
+//     console.log("Hello world")
+// }
+// console.log(typeof bigNumber);
+// memory in javascript there are two types of memory allocation (stack,heap)
+// stack(primitive) and heap (nonprimitive)
+let myyoutubename="nitya.com"
+let anothername=myyoutubename
+
+anothername="chai aur code"
+console.log(myyoutubename)
+console.log(anothername)
+
+let userone={
+    email:"nnityadubey@gmail.com",
+    upiid:"user@ybl"
 }
-const myFunction = function()
-{
-    console.log("Hello world")
-}
-console.log(typeof bigNumber);
+let usertwo=userone
+usertwo.email="keshav08@gmail.com"
+console.log(userone.email)
+console.log(usertwo.email)
