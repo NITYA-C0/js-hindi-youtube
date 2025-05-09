55c647291365a4e4be3c1f69a9f8810960a4b708
NITYA-C0 <154868587+NITYA-C0@users.noreply.github.com>
2025-05-09 08:05:59 +0000

    nums_and_maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,22 @@
+const score =400
+// console.log(score);
+const balance= new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+const otherNumber=23.8966
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds =10000
+// console.log(hundreds.toLocaleString('en-In'));
+// *********maths**********88//
+console.log(Math);
+console.log(Math.abs(4));
+// console.log(math.round(4.6));
+// console.log(math.ceil(4.2));
+// console.log(math.floor(4.2));
+// console.log(math.min(2,3,6,1,0));
+console.log(math.random());
+console.log(math.random()*10);
+
+
