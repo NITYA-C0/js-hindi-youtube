649b985e54598b7ea2a4a20bae5882f874e52ce4
NITYA-C0 <154868587+NITYA-C0@users.noreply.github.com>
2025-05-10 14:11:40 +0530

    dateandtime

---
+++ 01_basics/07_dates_and_time.js
@@ -0,0 +1,14 @@
+// let mydate = new Date()
+// console.log(mydate);
+
+// console.log(mydate.toString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleDateString());
+// console.log(typeof mydate);
+let mycreatedDate= new Date("2025-02-20")
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(mycreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
